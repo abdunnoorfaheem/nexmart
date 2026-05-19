dbd0bfbbe9e45b5459ae3fb080fc70b879a9ef21
abdunnoorfaheem <abdunnoor3030@gmail.com>
2026-05-19 18:30:34 +0600

    react router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import RootLayout from './components/layouts/RootLayout'
+import Home from './components/pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   <>
-  <h1>hello</h1>
+   <Routes>
+    <Route path='/' element={<RootLayout/>}>
+    <Route index element={<Home/>}/>
+    </Route>
+   </Routes>
   </>
   )
 }
